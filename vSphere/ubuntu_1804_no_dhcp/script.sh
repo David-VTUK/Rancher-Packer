df883eb9849954fd7b63b27f9728e58a5d27326a
David <david.holder@gmail.com>
2020-05-19 19:36:25 +0100

    Added Ubuntu 1804 no DHCP

---
+++ vSphere/ubuntu_1804_no_dhcp/script.sh
@@ -0,0 +1,12 @@
+# Reset the machine-id value. This has known to cause issues with DHCP
+echo -n > /etc/machine-id
+
+# Reset any existing cloud-init state
+cloud-init clean --logs
+
+# Force no DHCP usage
+echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+
+# Disable IPV6
+echo "net.ipv6.conf.all.disable_ipv6=1" > /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6=1" > /etc/sysctl.conf
