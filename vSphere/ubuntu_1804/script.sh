c7562f4b8bfb24f415d17b9efe188d2a2ecd6dbe
David-VTUK <david.holder@gmail.com>
2020-08-10 11:07:17 +0100

    Standardised Ubuntu scripts, disabled Swap

--- vSphere/ubuntu_1804/script.sh
+++ vSphere/ubuntu_1804/script.sh
@@ -4,18 +4,16 @@ apt-get update ; apt-get -y dist-upgrade
 apt-get -y autoremove
 apt-get -y clean
 
+# Disable swap - generally recommended for K8s, but otherwise enable it for other workloads
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
 # Reset the machine-id value. This has known to cause issues with DHCP
 #
-echo -n > /etc/machine-id
-
-# Create a swapfile
-#
-fallocate -l 2G /.swap
-chmod 0600 /.swap
-mkswap /.swap
-echo '/.swap    swap    swap    default    0   0' >> /etc/fstab
+truncate -s 0 /etc/machine-id
+rm /var/lib/dbus/machine-id
+ln -s /etc/machine-id /var/lib/dbus/machine-id
 
 # Reset any existing cloud-init state
 #
 cloud-init clean -s -l
-
--- vSphere/ubuntu_1804_no_dhcp/script.sh
+++ vSphere/ubuntu_1804_no_dhcp/script.sh
@@ -1,16 +1,19 @@
+# Apply updates and cleanup Apt cache
+#
+apt-get update ; apt-get -y dist-upgrade
+apt-get -y autoremove
+apt-get -y clean
+
+# Disable swap - generally recommended for K8s, but otherwise enable it for other workloads
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
 # Reset the machine-id value. This has known to cause issues with DHCP
-echo -n > /etc/machine-id
+#
+truncate -s 0 /etc/machine-id
+rm /var/lib/dbus/machine-id
+ln -s /etc/machine-id /var/lib/dbus/machine-id
 
 # Reset any existing cloud-init state
-cloud-init clean --logs
-
-# Force no DHCP usage
-echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
-
-# Disable IPV6
-echo "net.ipv6.conf.all.disable_ipv6=1" > /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6=1" > /etc/sysctl.conf
-
-# Disable Swap
-swapoff -a 
-sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+#
+cloud-init clean -s -l
